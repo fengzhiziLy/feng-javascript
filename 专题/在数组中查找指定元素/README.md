d4c80d84f19b8ef2fd77012396b152c0c551a810
fengzhiziLy <Ly332883899@163.com>
2018-08-20 15:47:35 +0800

    findIndex

--- 专题/在数组中查找指定元素/README.md
+++ 专题/在数组中查找指定元素/README.md
@@ -63,7 +63,7 @@ var findLastIndex = createIndexFinder(-1);
 
 #### sortedIndex
 
-假设这样：在一个排好序的数组中找到 value 对应的位置，保证插入数组后，依然保持有序的状态
+假设这样：在一个排好序的数组中找到`value`对应的位置，保证插入数组后，依然保持有序的状态
 
 效果为：
 
@@ -115,4 +115,106 @@ function sortedIndex(array, obj, iteratee, context) {
   }
   return high;
 }
+```
+
+#### indexOf
+
+```js
+function createIndexOfFinder(dir) {
+  return function (array, item) {
+    var length = array.length;
+    var index = dir > 0 ? 0 : length - 1;
+    for (; index >= 0 && index < length; index += dir) {
+      if (array[index] === item) return index;
+    }
+    return -1;
+  }
+}
+var indexOf = createIndexOfFinder(1);
+var lastIndexOf = createIndexOfFinder(-1);
+var result = indexOf([1, 2, 3, 4, 5], 2);
+console.log(result);
+```
+
+#### fromIndex
+
+`indexOf`的`fromIndex`:
+
+> indexOf() 方法可返回某个指定的字符串值在字符串中首次出现的位置
+> stringObject.indexOf(searchvalue,fromindex)
+> 设定开始查找的位置。如果该索引值大于或等于数组长度，意味着不会在数组里查找，返回 -1。如果参数中提供的索引值是一个负值，则将其作为数组末尾的一个抵消，即 -1 表示从最后一个元素开始查找，-2 表示从倒数第二个元素开始查找 ，以此类推。 注意：如果参数中提供的索引值是一个负值，仍然从前向后查询数组。如果抵消后的索引值仍小于 0，则整个数组都将会被查询。其默认值为 0
+
+`lastIndexOf`的`fromIndex`:
+
+> 从此位置开始逆向查找。默认为数组的长度减 1，即整个数组都被查找。如果该值大于或等于数组的长度，则整个数组会被查找。如果为负值，将其视为从数组末尾向前的偏移。即使该值为负，数组仍然会被从后向前查找。如果该值为负时，其绝对值大于数组长度，则方法返回 -1，即数组不会被查找
+
+```js
+function createIndexOfFinder(dir) {
+  return function(array, item, idx) {
+    var length = array.length;
+    var i = 0;
+    if (typeof idx == "number") {
+      if (dir > 0) {
+        i = idx >= 0 ? idx : Math.max(length + idx, 0);
+      } else {
+        length = idx >= 0 ? Math.min(idx + 1, length) : idx + length + 1;
+      }
+    }
+    for (idx = dir > 0 ? i : length - 1; idx >= 0 && idx <length; idx += dir) {
+      if (array[idx] === item) return idx;
+    }
+    return -1;
+  }
+}
+var indexOf = createIndexOfFinder(1);
+var lastIndexOf = createIndexOfFinder(-1);
+var result = indexOf([1, 2, 3, 4, 5], 2);
+console.log(result);
+```
+
+#### 优化
+
+在`underscore`中有两个优化，第一个是支持查找`NaN`:
+
+因为`NaN`不全等于`NaN`，所以原生的`indexOf`并不能找出`NaN`的下标。
+
+```js
+[1, NaN].indexOf(NaN) // -1
+```
+
+如何实现这个功能：就是从数组中找到符合条件的值的下标，就是一开始写的`findIndex`的
+
+第二个优化是支持对有序数组进行更快的二分查找：
+
+如果`indexOf`第三个参数不传开始搜索的下标值，而是一个布尔值`true`，就认为数组是一个排好序的数组，这时候，就会采用更快的二分法进行查找
+
+```js
+function createIndexOfFinder(dir, predicate, sortedIndex) {
+  return function (array, item, idx) {
+    var length = array.length;
+    var i = 0;
+    if (typeof idx == "number") {
+      if (dir > 0) {
+        i = idx >= 0 ? idx : Math.max(length + idx, 0);
+      } else {
+        length = idx >= 0 ? Math.min(idx + 1, length) : idx + length + 1;
+      }
+    } else if (sortedIndex && idx && length) {
+      idx = sortedIndex(array, item);
+      // 如果该插入的位置的值正好等于元素的值，说明是第一个符合要求的值
+      return array[idx] === item ? idx : -1;
+    }
+    // 判断是否是 NaN
+    if (item !== item) {
+      idx = predicate(array.slice(i, length), isNaN)
+      return idx >= 0 ? idx + i : -1;
+    }
+    for (idx = dir > 0 ? i : length - 1; idx >= 0 && idx < length; idx += dir) {
+      if (array[idx] === item) return idx;
+    }
+    return -1;
+  }
+}
+var indexOf = createIndexOfFinder(1, findIndex, sortedIndex);
+var lastIndexOf = createIndexOfFinder(-1, findLastIndex);
 ```
--- 专题/在数组中查找指定元素/index.js
+++ 专题/在数组中查找指定元素/index.js
@@ -46,24 +46,93 @@
 // console.log(sortedIndex([10, 20, 30, 40, 50], 35));
 
 
-function cb (func, context) {
-  if (context === void 0) return func;
-  return function () {
-    return func.apply(context, arguments);
+// function cb (func, context) {
+//   if (context === void 0) return func;
+//   return function () {
+//     return func.apply(context, arguments);
+//   }
+// }
+// function sortedIndex(array, obj, iteratee, context) {
+//   iteratee = cb(iteratee, context);
+//   var low = 0, high = array.length;
+//   while (low < high) {
+//     var mid = Math.floor((low + high) / 2);
+//     if (iteratee(array[mid]) < iteratee(obj)) low = mid + 1;
+//     else high = mid;
+//   }
+//   return high;
+// }
+// var stooges = [{name: 'stooge1', age: 10}, {name: 'stooge2', age: 30}];
+// var result = sortedIndex(stooges, {name: 'stooge3', age: 20}, function(stooge){
+//   return stooge.age
+// })
+// console.log(result);
+
+
+// function createIndexOfFinder(dir) {
+//   return function (array, item) {
+//     var length = array.length;
+//     var index = dir > 0 ? 0 : length - 1;
+//     for (; index >= 0 && index < length; index += dir) {
+//       if (array[index] === item) return index;
+//     }
+//     return -1;
+//   }
+// }
+// var indexOf = createIndexOfFinder(1);
+// var lastIndexOf = createIndexOfFinder(-1);
+// var result = indexOf([1, 2, 3, 4, 5], 2);
+// console.log(result);
+
+
+// function createIndexOfFinder(dir) {
+//   return function(array, item, idx) {
+//     var length = array.length;
+//     var i = 0;
+//     if (typeof idx == "number") {
+//       if (dir > 0) {
+//         i = idx >= 0 ? idx : Math.max(length + idx, 0);
+//       } else {
+//         length = idx >= 0 ? Math.min(idx + 1, length) : idx + length + 1;
+//       }
+//     }
+//     for (idx = dir > 0 ? i : length - 1; idx >= 0 && idx <length; idx += dir) {
+//       if (array[idx] === item) return idx;
+//     }
+//     return -1;
+//   }
+// }
+// var indexOf = createIndexOfFinder(1);
+// var lastIndexOf = createIndexOfFinder(-1);
+// var result = indexOf([1, 2, 3, 4, 5], 2);
+// console.log(result);
+
+
+function createIndexOfFinder(dir, predicate, sortedIndex) {
+  return function (array, item, idx) {
+    var length = array.length;
+    var i = 0;
+    if (typeof idx == "number") {
+      if (dir > 0) {
+        i = idx >= 0 ? idx : Math.max(length + idx, 0);
+      } else {
+        length = idx >= 0 ? Math.min(idx + 1, length) : idx + length + 1;
+      }
+    } else if (sortedIndex && idx && length) {
+      idx = sortedIndex(array, item);
+      // 如果该插入的位置的值正好等于元素的值，说明是第一个符合要求的值
+      return array[idx] === item ? idx : -1;
+    }
+    // 判断是否是 NaN
+    if (item !== item) {
+      idx = predicate(array.slice(i, length), isNaN)
+      return idx >= 0 ? idx + i : -1;
+    }
+    for (idx = dir > 0 ? i : length - 1; idx >= 0 && idx < length; idx += dir) {
+      if (array[idx] === item) return idx;
+    }
+    return -1;
   }
 }
-function sortedIndex(array, obj, iteratee, context) {
-  iteratee = cb(iteratee, context);
-  var low = 0, high = array.length;
-  while (low < high) {
-    var mid = Math.floor((low + high) / 2);
-    if (iteratee(array[mid]) < iteratee(obj)) low = mid + 1;
-    else high = mid;
-  }
-  return high;
-}
-var stooges = [{name: 'stooge1', age: 10}, {name: 'stooge2', age: 30}];
-var result = sortedIndex(stooges, {name: 'stooge3', age: 20}, function(stooge){
-  return stooge.age
-})
-console.log(result);
+var indexOf = createIndexOfFinder(1, findIndex, sortedIndex);
+var lastIndexOf = createIndexOfFinder(-1, findLastIndex);
