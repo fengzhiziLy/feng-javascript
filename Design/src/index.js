7ad1d266d71153fe30e5483496628c9d74faddf5
fengzhiziLy <Ly332883899@163.com>
2018-09-04 11:52:54 +0800

    Vue

--- Design/src/index.js
+++ Design/src/index.js
@@ -1,22 +1,44 @@
-class ReadImg {
-  constructor(fileName) {
-    this.fileName = fileName
-    this.loadFromDisk()
+// 主题，接收状态变化，触发每个观察者
+class Subject {
+  constructor() {
+      this.state = 0
+      this.observers = []
   }
-  display() {
-    console.log('display...' + this.fileName)
+  getState() {
+      return this.state
   }
-  loadFromDisk() {
-    console.log('loading...' + this.fileName)
+  setState(state) {
+      this.state = state
+      this.notifyAllObservers()
+  }
+  attach(observer) {
+      this.observers.push(observer)
+  }
+  notifyAllObservers() {
+      this.observers.forEach(observer => {
+          observer.update()
+      })
   }
 }
-class ProxyImg {
-  constructor(fileName) {
-    this.realImg = new ReadImg(fileName)
+
+// 观察者，等待被触发
+class Observer {
+  constructor(name, subject) {
+      this.name = name
+      this.subject = subject
+      this.subject.attach(this)
   }
-  display() {
-    this.realImg.display()
+  update() {
+      console.log(`${this.name} update, state: ${this.subject.getState()}`)
   }
 }
-let proxyImg = new ProxyImg('1.png')
-proxyImg.display()
+
+// 测试代码
+let s = new Subject()
+let o1 = new Observer('o1', s)
+let o2 = new Observer('o2', s)
+let o3 = new Observer('o3', s)
+
+s.setState(1)
+s.setState(2)
+s.setState(3)
